e33f9efc651046ab25737d71ed898d90bac71f6b
kapasovamir <kapasovamir777@gmail.com>
2026-01-08 02:26:48 -0800

    Added

--- main.py
+++ main.py
@@ -1,63 +1,101 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QPainter, QPen, QColor
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow,QPushButton
+from PyQt5.QtGui import QPainter, QPen, QColor, QFont
+from PyQt5.QtCore import Qt, QTimer,QUrl
+from PyQt5.QtMultimedia import QSound,QMediaPlayer,QMediaContent
+
+time_m = int(input("Enter the time in m: "))
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # заголовок окна
-        self.setWindowTitle("Pomadaro")
+        self.setWindowTitle("Pomodoro")
         self.setMinimumSize(400, 400)
+        # Темный фон
+        self.setStyleSheet("background-color: #2b2b2b;")
 
-        self.progress = 1.0
+        # Аудио плеер настройка
+        self.player = QMediaPlayer()
+        # Расположение звука
+        sound_path = "/Users/kapasovamir/PycharmProjects/MiniProject/sound/02599.mp3"
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(sound_path)))
 
-        # Анимация Таймера
+        # Настройки времени
+        self.total_seconds = time_m * 60
+        self.remaining_seconds = self.total_seconds
+
+        # Таймер срабатывает раз (1000 мс)
         self.timer = QTimer()
-        self.timer.timeout.connect(self.updateTimer)
-        self.timer.start(10)
+        self.timer.timeout.connect(self.update_timer)
+        self.timer.start(1000)
 
-    def updateTimer(self):
-        if self.progress > 0:
-            self.progress -= 0.000006666
-            self.update()
-        else:
+        self.StartButton = QPushButton(self)
+        self.StartButton.setText("Start")
+        self.StartButton.move(75, 250)
+        self.StartButton.clicked.connect(self.timer.start)
+
+        self.StopButton = QPushButton(self)
+        self.StopButton.setText("Stop")
+        self.StopButton.move(220, 250)
+        self.StopButton.clicked.connect(self.timer.stop)
+
+
+    # Функция для анимация таймера
+    def update_timer(self):
+        if self.remaining_seconds > 0:
+            self.remaining_seconds -= 1
+            self.update() # Перерисовать окно
+        if self.remaining_seconds == 0:
             self.timer.stop()
+            self.player.play() # Вывод звука
+            self.total_seconds = time_m * 60
+            self.remaining_seconds = self.total_seconds
 
     def paintEvent(self, event):
+
+        # Значение окна берет
         width = self.width()
         height = self.height()
+
+        # Отступ от краев
         side = min(width, height) - 50
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
+        x = (width - side) // 2
+        y = (height - side) // 2
 
-        pen = QPen()
-        pen.setWidth(5)
-        pen.setCapStyle(Qt.RoundCap)
+        painter = QPainter(self) # Холст
+        painter.setRenderHint(QPainter.Antialiasing) # Сглаживание холста
 
-        pen.setColor(QColor("#e0e0e0"))
+        # Серый фоновый круг
+        pen = QPen(QColor("#444444"), 8)
         painter.setPen(pen)
-        painter.drawEllipse((width - side) // 2, (height - side) // 2, side, side)
+        painter.drawEllipse(x, y, side, side)
 
-        pen.setColor(QColor("#ff5555"))
-        painter.setPen(pen)
+        # Красную круг над фоновым кругом
+        if self.total_seconds > 0:
+            progress = self.remaining_seconds / self.total_seconds
+            pen.setColor(QColor("#ff5555"))
+            pen.setCapStyle(Qt.RoundCap)
+            painter.setPen(pen)
 
-        start_angle = 90*16
-        span_angle = int(self.progress * 360 * 16)
+            start_angle = 90 * 16 # Головка линий
+            span_angle = int(progress * 360 * 16) # Конец линий
+            painter.drawArc(x, y, side, side, start_angle, span_angle)
 
-        painter.drawArc((width - side) // 2, (height - side) // 2, side, side,
-                        start_angle, span_angle)
+        # Текст временни (ММ:СС)
+        minutes = self.remaining_seconds // 60
+        seconds = self.remaining_seconds % 60
+        time_text = f"{minutes:02}:{seconds:02}"
 
-if __name__ == "__main__":
-    # объект приложения
+        painter.setFont(QFont("Arial", 40, QFont.Bold)) # Значение текста
+        painter.setPen(QColor("#FFFFFF"))
+        painter.drawText(self.rect(), Qt.AlignCenter, time_text) # Вывод по центру таймер
+
+
+def main():
     app = QApplication(sys.argv)
-
-    # экземпляр нашего класса
     window = MyWindow()
-
-    # Показываем окно
     window.show()
-
-    # Запускаем цикл обработки событий
     sys.exit(app.exec_())
+if __name__ == "__main__":
+    main()
